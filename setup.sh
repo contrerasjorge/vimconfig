64bc5a5a528731653d2e85bf0ef33803ecc320bd
Jorge Contreras <jorge.contreras789@gmail.com>
2020-04-04 14:36:49 -0400

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sh
+
+set -ex
+
+ln -s ~/.vim/vimrc ~/.vimrc
+
+mkdir -p ~/.config/nvim
+ln -s ~/.vim/nvim_init.vim ~/.config/nvim/init.vim
+
+mkdir -p ~/.vim/tmp/undo
+mkdir -p ~/.vim/tmp/backup
